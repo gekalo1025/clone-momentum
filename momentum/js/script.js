b5da3a1f748e17ea9b16a00655afd0d489e959a5
Roman <gekalo1025@gmail.com>
2022-07-20 12:28:39 +0300

    feat: function getTimeOfDay and showGreeting

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -8,6 +8,7 @@ function  showTime (){
     const currentTime = DATE.toLocaleTimeString()
     time.textContent = currentTime;
     showData();
+    getTimeOfDay();
     setTimeout(showTime,1000);
 }
 function showData(){
@@ -19,3 +20,37 @@ function showData(){
     
 }
 showTime();
+
+//Greetings///
+
+const greeting = document.querySelector('.greeting');
+
+function getTimeOfDay(){
+let timeOfDay;
+const DATE = new Date();
+const hours = DATE.getHours();
+if(hours>=6 && hours<12){timeOfDay = 'morning'}
+if(hours>=12 && hours<18){timeOfDay = 'day'}
+if(hours>=18 && hours<0){timeOfDay = 'evening'}
+if(hours>=0 && hours<6){timeOfDay = 'night'}
+return timeOfDay
+}
+
+function showGreeting() {
+        greeting.textContent=`Good ${getTimeOfDay()}`
+    }
+showGreeting();
+////сохранение переменных в local storege при событии beforeunload
+const name = document.querySelector('.name')
+function setLocalStorage() {
+    localStorage.setItem('name',name.value);
+  }
+  window.addEventListener('beforeunload', setLocalStorage)
+
+  //при загрузки если есть ключ name добавляет ее value
+  function getLocalStorage() {
+    if(localStorage.getItem('name')) {
+      name.value = localStorage.getItem('name');
+    }
+  }
+  window.addEventListener('load', getLocalStorage)
